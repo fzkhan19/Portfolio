f5058142ec322caab89385999a46c01b826a4abb
Faiz Khan <faiz.khan@everestek.com>
2023-03-16 21:56:02 +0530

    Almost everything complete just stars and earth left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,10 +15,26 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Faiz Khan</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop cool web applications, that solves <span className="text-red-700">Not So Cool</span> Problems!</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 tracking-tight`}>
+            I develop <span className="text-blue-300 font-bold">Cool</span> web applications, to solve{" "}
+            <span className="text-red-400 font-bold ">Not So Cool</span> Problems!
+          </p>
+          
         </div>
       </div>
-        <ComputersCanvas/>
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop'}}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Faiz Khan&nbsp;<span className="sm:block hidden text-violet-300">| Portfolio</span>
+            Faiz Khan&nbsp;<span className="sm:block hidden text-violet-400">| Portfolio</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
